8080b69d0a368c30090c4bcb7b0e05cc47291d1c
Joanna <joanna.dyszkiewicz@pagepro.pl>
2018-11-15 22:12:47 +0100

    Hints

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,28 @@ var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
 var watch = require('gulp-watch')
 var flatten = require('gulp-flatten')
+var sourcemaps = require('gulp-sourcemaps')
 
 var browserSync = require('browser-sync').create()
 
-gulp.task('watchapp', ['sass'], function() {
-    browserSync.init({
-        port: process.env.PORT || 4500,
-        server: './',
-    });
+gulp.task('watchapp', ['sass'], function () {
+  browserSync.init({
+    port: process.env.PORT || 4500,
+    server: './'
+  })
 
-    gulp.watch('scss/**/*.scss', ['sass']).on('change', browserSync.reload);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-});
+  watch('scss/**/*.scss', ['sass']).on('change', browserSync.reload)
+  watch('./*.html').on('change', browserSync.reload)
+})
 
 gulp.task('sass', function () {   // definicja taska kompilującego sass
   return gulp.src('scss/**/*.scss') // wzorzec plików ktore mają być skompilowane (pliki z rozszerzeniem *.scss ze wszystkich podfolderów i folderu Content/app)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({ browsers: ['> 0%'], grid: true }))
-        .pipe(flatten())
-        .pipe(gulp.dest('.')) // ścieżka gdzie zapisać wynik taska czyli skompilowane pliki css
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['> 0%'], grid: true }))
+    .pipe(sourcemaps.write())
+    .pipe(flatten())
+    .pipe(gulp.dest('.')) // ścieżka gdzie zapisać wynik taska czyli skompilowane pliki css
 })
 
 gulp.task('default', ['watchapp'])
